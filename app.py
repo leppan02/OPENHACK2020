b74cfe436f054dad8d7aa95ad7ce00dc228240d2
axelamit <axel.v.nilsson@gmail.com>
2020-11-06 22:10:50 +0100

    add html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-    print ("testing")
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=1234)
