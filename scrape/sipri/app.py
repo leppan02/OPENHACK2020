a0623c5941deaf0c24282b5cbcb451915057edbe
leopoldh <leopoldh@kth.se>
2020-11-06 22:01:20 +0100

    can parse from sipri

--- scrape/sipri/app.py
+++ scrape/sipri/app.py
@@ -1,9 +1,9 @@
 
 import requests, pandas, io
-def getCSV(country_code='SWE'):
+def getCSV(country_code='USA'):
     payload = {'import_or_export': 'export',
     'country_code': country_code,
-    'low_year': 2018,
+    'low_year': 2016,
     'high_year': 2019,
     'summarize': 'country',
     'filetype': 'csv',
@@ -13,6 +13,6 @@ def getCSV(country_code='SWE'):
 
 if __name__ == '__main__':
     csv = getCSV()
-    dataframe = pandas.read_csv(io.BytesIO(csv))
-    print(len(dataframe))
+    dataframe = pandas.read_csv(io.BytesIO(csv), names=['name', '2016', '2017', '2018', '2019', 'tot', 'nothing'])
+    dataframe = dataframe.drop(columns=['tot', 'nothing']).fillna(0).drop([len(dataframe)-1,len(dataframe)-2, len(dataframe)-3]).drop(dataframe.index[:11]).reset_index(drop=True)
     print(dataframe)
