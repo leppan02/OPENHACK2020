5aceaf5d4488156d3af204ed219889ad3d8864d2
loovjo <jonathan.loov@gmail.com>
2020-11-07 00:18:20 +0100

    setup database

---
+++ setup-db.sql
@@ -0,0 +1,2 @@
+CREATE TABLE trades (id SERIAL PRIMARY KEY, country_from varchar(500), country_to varchar(500), thing varchar(500), amount INT, trade_start DATE, trade_end DATE);
+INSERT INTO trades (country_from, country_to, thing, amount, trade_start, trade_end) VALUES ("Göteborg", "Alfaganistan", "bomb", current_date - interval '20:00:00', current_date);
