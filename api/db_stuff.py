c3f2c960adc2fb9d99ba084ef96c9cec5140e401
loovjo <jonathan.loov@gmail.com>
2020-11-07 21:20:26 +0100

    Split api into db-part

---
+++ api/db_stuff.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import hashlib
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:docker@postgres/hh"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Trade(db.Model):
+    __tablename__ = 'trades'
+
+    id = db.Column(db.Integer, primary_key=True)
+    country_from = db.Column(db.String())
+    country_to = db.Column(db.String())
+    weapon_name = db.Column(db.String())
+    amount = db.Column(db.Integer())
+    trade_start = db.Column(db.Date())
+    trade_end = db.Column(db.Date())
+    is_verified = db.Column(db.Boolean())
+    source = db.Column(db.String())
+
+    def __init__(self, country_from, country_to, weapon_name, amount, trade_start, trade_end, is_verified, source): 
+        self.country_from = country_from
+        self.country_to = country_to
+        self.weapon_name = weapon_name
+        self.amount = amount
+        self.trade_start = trade_start
+        self.trade_end = trade_end
+        self.is_verified = is_verified
+        self.source = source
+
+class Weapon(db.Model):
+    __tablename__ = 'weapons'
+
+    weapon_name = db.Column(db.String(), primary_key=True)
+    category = db.Column(db.String())
+
+    def __init__(self, weapon_name, category):
+        self.weapon_name = weapon_name
+        self.category = category
+
+class Api(db.Model):
+    __tablename__ = 'api'
+
+    key = db.Column(db.String(), primary_key=True)
+    email = db.Column(db.String())
+
+    def __init__(self, email):
+        self.email = email
+        self.key = hashlib.md5(hex(random.randint(0,1000000000000000000))).hexdigest()
+
--- api/main.py
+++ api/main.py
@@ -6,54 +6,7 @@ from datetime import datetime
 import hashlib
 import random
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:docker@postgres/hh"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class Trade(db.Model):
-    __tablename__ = 'trades'
-
-    id = db.Column(db.Integer, primary_key=True)
-    country_from = db.Column(db.String())
-    country_to = db.Column(db.String())
-    weapon_name = db.Column(db.String())
-    amount = db.Column(db.Integer())
-    trade_start = db.Column(db.Date())
-    trade_end = db.Column(db.Date())
-    is_verified = db.Column(db.Boolean())
-    source = db.Column(db.String())
-
-    def __init__(self, country_from, country_to, weapon_name, amount, trade_start, trade_end, is_verified, source): 
-        self.country_from = country_from
-        self.country_to = country_to
-        self.weapon_name = weapon_name
-        self.amount = amount
-        self.trade_start = trade_start
-        self.trade_end = trade_end
-        self.is_verified = is_verified
-        self.source = source
-
-class Weapon(db.Model):
-    __tablename__ = 'weapons'
-
-    weapon_name = db.Column(db.String(), primary_key=True)
-    category = db.Column(db.String())
-
-    def __init__(self, weapon_name, category):
-        self.weapon_name = weapon_name
-        self.category = category
-
-class Api(db.Model):
-    __tablename__ = 'api'
-
-    key = db.Column(db.String(), primary_key=True)
-    email = db.Column(db.String())
-
-    def __init__(self, email):
-        self.email = email
-        self.key = hashlib.md5(hex(random.randint(0,1000000000000000000))).hexdigest()
+from db_stuff import *
 
 def parse_date(data):
     return datetime.strptime(data, "%Y-%m-%d").date()
@@ -149,7 +102,6 @@ def generate():
         db.session.commit()
         return new_key.key
     return "Expected JSON"
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=1234)
