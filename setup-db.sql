e6fa27d300548b01d56cad94148edd2cdecaa7bc
loovjo <jonathan.loov@gmail.com>
2020-11-07 01:43:48 +0100

    new table

--- setup-db.sql
+++ setup-db.sql
@@ -1,4 +1,18 @@
 CREATE DATABASE hh;
 CONNECT TO hh;
-CREATE TABLE trades (id SERIAL PRIMARY KEY, country_from varchar(500), country_to varchar(500), thing varchar(500), amount INT, trade_start DATE, trade_end DATE);
-INSERT INTO trades (country_from, country_to, thing, amount, trade_start, trade_end) VALUES ('Göteborg', 'Alfaganistan', 'bomb', 100, current_date - interval '20:00:00', current_date);
+
+CREATE TABLE trades (
+    id SERIAL PRIMARY KEY,
+    country_from varchar(500),
+    country_to varchar(500),
+    weapon_name varchar(500),
+    amount INT,
+    trade_start DATE,
+    trade_end DATE
+);
+
+CREATE TABLE weapons (
+    weapon_name varchar(500) PRIMARY KEY NOT NULL,
+    category varchar(500),
+);
+
